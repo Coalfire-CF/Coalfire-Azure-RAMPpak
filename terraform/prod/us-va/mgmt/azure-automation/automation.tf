18088a8b3a9855e8de5f31a6133741bec5be2c92
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2023-09-28 20:31:38 -0400

    cleaned up automation.tf

--- terraform/prod/us-va/mgmt/azure-automation/automation.tf
+++ terraform/prod/us-va/mgmt/azure-automation/automation.tf
@@ -1,28 +1,5 @@
-# resource "azurerm_automation_account" "va-aa" {
-#   name                = "${local.resource_prefix}-aa"
-#   location            = var.location
-#   resource_group_name = data.terraform_remote_state.setup.outputs.management_rg_name
-#   sku_name            = "Basic"
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-
-#   tags = merge({
-#     Function    = "Automation"
-#     Plane       = "Management"
-#     Environment = "Production"
-#   }, var.global_tags, var.regional_tags, local.global_local_tags)
-# }
-
-# resource "azurerm_log_analytics_linked_service" "aa_la_link" {
-#   resource_group_name = data.terraform_remote_state.core.outputs.core_rg_name
-#   workspace_id        = data.terraform_remote_state.core.outputs.core_la_id
-#   read_access_id      = azurerm_automation_account.va-aa.id
-# }
-
 module "va-aa" {
-  source                     = "github.com/Coalfire-CF/terraform-azurerm-automation-account"
+  source                     = "github.com/Coalfire-CF/terraform-azurerm-automation-account?ref=OC"
   automation_account_name    = "${local.resource_prefix}-aa"
   resource_group_name        = data.terraform_remote_state.setup.outputs.management_rg_name
   location                   = var.location

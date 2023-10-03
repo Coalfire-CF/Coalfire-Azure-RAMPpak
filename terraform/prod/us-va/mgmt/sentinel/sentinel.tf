728a048e0b3aa008488bd93f518cb1383225eb14
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2023-10-03 16:58:30 -0400

    removed comments

--- terraform/prod/us-va/mgmt/sentinel/sentinel.tf
+++ terraform/prod/us-va/mgmt/sentinel/sentinel.tf
@@ -15,30 +15,3 @@ module "sentinel" {
   }, var.regional_tags, local.global_local_tags)
 }
 
-
-
-
-# resource "azurerm_log_analytics_solution" "tm-sentinel" {
-#   solution_name         = "SecurityInsights"
-#   location              = var.location
-#   resource_group_name   = data.terraform_remote_state.core.outputs.core_rg_name
-#   workspace_resource_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
-#   workspace_name        = data.terraform_remote_state.core.outputs.core_la_workspace_name
-
-#   plan {
-#     publisher = "Microsoft"
-#     product   = "OMSGallery/SecurityInsights"
-#   }
-# }
-
-# # It appears enabling this manually may not be required. Initial deployment showed this getting enabled based on the logs
-# # already existing in the workspace
-# # resource "azurerm_sentinel_data_connector_azure_active_directory" "sentinel-aad" {
-# #   name                       = "AAD-Connector"
-# #   log_analytics_workspace_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
-# # }
-
-# resource "azurerm_sentinel_data_connector_threat_intelligence" "sentinel-taxii" {
-#   name                       = "TAXII"
-#   log_analytics_workspace_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
-# }

dade4776108d3b8100c13c013c13851c6fb8aa93
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-03-07 14:26:06 -0500

    update security core required_providers.azurerm

--- terraform/prod/us-va/security-core/tstate.tf
+++ terraform/prod/us-va/security-core/tstate.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.61.0"
+      version = "~>4.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
